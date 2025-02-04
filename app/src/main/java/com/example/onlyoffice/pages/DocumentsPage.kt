6d87392eea2da277597556c2d4244d6453bbacb4
LilChihba <danila-yurov@mail.ru>
2025-02-04 17:04:20 +0300

    created NavigationBar

---
+++ app/src/main/java/com/example/onlyoffice/pages/DocumentsPage.kt
@@ -0,0 +1,24 @@
+package com.example.onlyoffice.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun DocumentsPage() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Documents Page"
+        )
+    }
+}
